8c9baabf4e07b8e7d7e10bc87a8262419348fae5
Iago Moreira <iagomoreira2353@gmail.com>
2024-09-14 13:18:59 -0300

    Delete 01_exercicio/respostas/_04_Questao.js

--- 01_exercicio/respostas/_04_Questao.js
+++
@@ -1,8 +0,0 @@
-function restoDaDivisao(dividendo, divisor) {
-  return dividendo % divisor;
-}
-
-console.log(restoDaDivisao(5, 2));
-
-/* Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado 
-e o resto da divisão destes dois valores */
