4b97732368059ca877374e85330f499986e57ae3
Iago Moreira <iagomoreira2353@gmail.com>
2024-09-15 19:38:54 -0300

    Comecei o segundo exercício

---
+++ 02_exercicio/respostas/01_questao.js
@@ -0,0 +1,9 @@
+let cumprimentar = (valor) => {
+  let valorConcatenado = "Olá, " + valor + "!";
+  console.log(valorConcatenado);
+};
+
+cumprimentar("Ana");
+
+/* Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a
+função) e com ponto de exclamação "!" no final */
---
+++ 02_exercicio/respostas/02_questao.js
@@ -0,0 +1,8 @@
+let converterIdadeEmAnosParaDias = (idade) => {
+  let dias = idade * 365;
+  console.log(`${dias} dias de idade`);
+};
+
+converterIdadeEmAnosParaDias(25);
+
+/* Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias. Obs: considere que um ano tem 365 dias. Desconsidere anos bissextos (com 366 dias) e desconsidere também dias decorridos desde o último aniversário */
